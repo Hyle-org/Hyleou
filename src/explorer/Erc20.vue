9709162d87d1da83d10330603c83c6d47761da03
Bertrand Darbon <bertrand.darbon@gmail.com>
2024-09-27 10:12:37 +0200

    🎨 Run prettier (#9)
    prettier 3.3.3
    
    prettier -w .

--- src/explorer/Erc20.vue
+++ src/explorer/Erc20.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { ref, computed, reactive, watchEffect } from 'vue';
-import { Erc20Parser } from 'hyle-js';
-import { transactionsStore } from '@/explorer/data';
+import { ref, computed, reactive, watchEffect } from "vue";
+import { Erc20Parser } from "hyle-js";
+import { transactionsStore } from "@/explorer/data";
 const props = defineProps<{
     contract_name: string;
 }>();
 
 const transactions = computed(() => {
-    const txs = Object.values(transactionsStore.value.transactionData).filter(tx => tx.contracts?.includes(props.contract_name))
+    const txs = Object.values(transactionsStore.value.transactionData).filter((tx) => tx.contracts?.includes(props.contract_name));
     txs.sort((a, b) => a.height - b.height + a.index - b.index);
     return txs;
 });
@@ -16,7 +16,7 @@ const contractData = ref(new Erc20Parser(props.contract_name));
 
 watchEffect(() => {
     contractData.value = reactive(new Erc20Parser(props.contract_name));
-    transactions.value.forEach(tx => {
+    transactions.value.forEach((tx) => {
         if (!tx.type) {
             transactionsStore.value.loadTransactionData(tx.hash); // inefficient if we do this many times.
             return;
@@ -25,16 +25,14 @@ watchEffect(() => {
         if (tx.status !== "sequenced") contractData.value.settleTx(tx.hash, tx.status === "success");
     });
 });
-
-
 </script>
 
 <template>
     <h2>ERC20 balances</h2>
     <div>
-        <p v-for="balance, address in contractData.balancesSettled" :key="address" class="balance">
-            {{ address }}: {{ balance }} <span v-if="contractData.pendingPerAccount[address]?.length > 0">(pending: {{
-            contractData.balancesPending[address] }})</span>
+        <p v-for="(balance, address) in contractData.balancesSettled" :key="address" class="balance">
+            {{ address }}: {{ balance }}
+            <span v-if="contractData.pendingPerAccount[address]?.length > 0">(pending: {{ contractData.balancesPending[address] }})</span>
         </p>
     </div>
 </template>
--- src/explorer/RegisterContract.vue
+++ src/explorer/RegisterContract.vue
@@ -1,29 +1,33 @@
 <script setup lang="ts">
-import { checkContractExists, checkTxStatus, registerContract, setupCosmos } from 'hyle-js';
-import Header from '@/explorer/Header.vue'
-import { ref } from 'vue';
-import { getNetworkRpcUrl } from 'hyle-js';
-import { network } from '@/explorer/data';
+import { checkContractExists, checkTxStatus, registerContract, setupCosmos } from "hyle-js";
+import Header from "@/explorer/Header.vue";
+import { ref } from "vue";
+import { getNetworkRpcUrl } from "hyle-js";
+import { network } from "@/explorer/data";
 
 const isCosmosReady = setupCosmos(`${getNetworkRpcUrl(network.value)}`);
 
-const name = ref<string>('');
-const verifierType = ref<string>('risczero');
-const programId = ref<string>('');
-const initialStateDigest = ref<string>('')
+const name = ref<string>("");
+const verifierType = ref<string>("risczero");
+const programId = ref<string>("");
+const initialStateDigest = ref<string>("");
 
 const stringToBytes = (val: string) => {
     try {
         // Parse as hex string first
         if (val.length % 2 !== 0) {
-            throw new Error('Invalid hex string');
+            throw new Error("Invalid hex string");
         }
         return new Uint8Array(val.match(/.{2}/g)!.map((byte) => +BigInt(`0x${byte}`).toString(10)));
     } catch (e) {
         // Parse as base64
-        return new Uint8Array(atob(val).split('').map((c) => c.charCodeAt(0)));
+        return new Uint8Array(
+            atob(val)
+                .split("")
+                .map((c) => c.charCodeAt(0)),
+        );
     }
-}
+};
 
 const registerError = ref<string | null>(null);
 const registerValue = ref<any | null>(null);
@@ -36,7 +40,7 @@ const submitForm = async () => {
     registerError.value = null;
     await isCosmosReady;
     if (await checkContractExists(network.value, name.value)) {
-        registerError.value = 'Contract already exists';
+        registerError.value = "Contract already exists";
         registering.value = false;
         return;
     }
@@ -46,19 +50,17 @@ const submitForm = async () => {
             verifierType.value,
             name.value,
             stringToBytes(programId.value),
-            stringToBytes(initialStateDigest.value)
+            stringToBytes(initialStateDigest.value),
         );
         await new Promise((resolve) => setTimeout(resolve, 1000));
         const code = await checkTxStatus(tx.transactionHash);
-        if (code.status === 'success')
-            registerValue.value = tx.transactionHash;
-        else
-            registerError.value = code.error!;
+        if (code.status === "success") registerValue.value = tx.transactionHash;
+        else registerError.value = code.error!;
     } catch (e: any) {
         registerError.value = e.message;
     }
     registering.value = false;
-}
+};
 </script>
 
 <template>
@@ -69,11 +71,11 @@ const submitForm = async () => {
                 <h1>Register a new contract</h1>
                 <form class="flex flex-col my-4 gap-4">
                     <p>
-                        <label for="name">Name</label><br>
-                        <input type="text" id="name" name="name" placeholder="my_contract" v-model="name" required>
+                        <label for="name">Name</label><br />
+                        <input type="text" id="name" name="name" placeholder="my_contract" v-model="name" required />
                     </p>
                     <p>
-                        <label for="verifier_type">Verifier Type</label><br>
+                        <label for="verifier_type">Verifier Type</label><br />
                         <select id="verifier_type" name="verifier_type" v-model="verifierType" required>
                             <option value="risczero">RISC Zero</option>
                             <option value="cairo">Cairo VM</option>
@@ -82,14 +84,27 @@ const submitForm = async () => {
                         </select>
                     </p>
                     <p>
-                        <label for="program_id">Program ID</label><br>
-                        <textarea id="program_id" name="program_id" placeholder="FOOBAR" v-model="programId"
-                            class="resize" rows="1" required />
+                        <label for="program_id">Program ID</label><br />
+                        <textarea
+                            id="program_id"
+                            name="program_id"
+                            placeholder="FOOBAR"
+                            v-model="programId"
+                            class="resize"
+                            rows="1"
+                            required
+                        />
                     </p>
                     <p>
-                        <label for="initial_state_digest">Initial State Digest</label><br>
-                        <input type="text" id="initial_state_digest" name="initial_state_digest" placeholder="00"
-                            v-model="initialStateDigest" required>
+                        <label for="initial_state_digest">Initial State Digest</label><br />
+                        <input
+                            type="text"
+                            id="initial_state_digest"
+                            name="initial_state_digest"
+                            placeholder="00"
+                            v-model="initialStateDigest"
+                            required
+                        />
                     </p>
                     <div>
                         <button class="rounded-lg my-4 w-auto" @click.prevent="submitForm">Register</button>
